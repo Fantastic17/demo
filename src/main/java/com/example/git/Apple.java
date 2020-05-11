c7bb2e6432be9bccd0b2663513699f92b7706918
tom_glb <GoodNight@atguigu.com>
2020-05-11 17:26:33 +0800

    first commit by idea

---
+++ src/main/java/com/example/git/Apple.java
@@ -0,0 +1,8 @@
+package com.example.git;
+
+/**
+ * @author wind
+ * @create 2020-05-11 17:24
+ */
+public class Apple {
+}
---
+++ src/main/java/com/example/git/Happy.java
@@ -0,0 +1,12 @@
+package com.example.git;
+
+/**
+ * @author wind
+ * @create 2020-05-11 17:24
+ */
+public class Happy {
+
+    public static void main(String[] args) {
+        System.out.println("left ...");
+    }
+}
