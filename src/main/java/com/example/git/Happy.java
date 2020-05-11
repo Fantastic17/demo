52019ce90ecf92f91fca63c95db13da9b09ae1fb
tom_glb <GoodNight@atguigu.com>
2020-05-11 17:41:09 +0800

    edit on branch hot_fix

--- src/main/java/com/example/git/Happy.java
+++ src/main/java/com/example/git/Happy.java
@@ -9,5 +9,6 @@ public class Happy {
     public static void main(String[] args) {
         System.out.println("left ...");
         System.out.println("hot fix ...");
+        System.out.println("hhhhhhhhhhhhhhhh");
     }
 }
