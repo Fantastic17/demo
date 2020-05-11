feb1564fb17075a84b66516d5200e1ec241e7c1e
tom_glb <GoodNight@atguigu.com>
2020-05-11 17:28:55 +0800

    edit on branch hot_fix

---
+++ src/main/java/com/example/git/Apple.java
@@ -0,0 +1,8 @@
+package com.example.git;
+
+/**
+ * @author wind
+ * @create 2020-05-11 17:27
+ */
+public class Apple {
+}
---
+++ src/main/java/com/example/git/Happy.java
@@ -0,0 +1,13 @@
+package com.example.git;
+
+/**
+ * @author wind
+ * @create 2020-05-11 17:27
+ */
+public class Happy {
+
+    public static void main(String[] args) {
+        System.out.println("left ...");
+        System.out.println("hot fix ...");
+    }
+}
